1b0e014ba60c3e48ee2ae49501c4884dd17cae4f
hsh5206 <hyunhsh5206@gmail.com>
2022-11-08 17:29:41 +0900

    section1 ~ section3

---
+++ Source/FirstProject/MyObject.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyObject.h"
+
+UMyObject::UMyObject() {
+	MyFloat = 5.1f;
+}
+
+void UMyObject::MyFunction()
+{
+}
